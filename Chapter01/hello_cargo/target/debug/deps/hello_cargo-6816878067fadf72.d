70bf798879a8d9fb2fa3a2feffdbd857568c7485
Evan Kiefl <kiefl.evan@gmail.com>
2020-02-08 12:51:09 -0600

    done chapter 1

---
+++ Chapter01/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d
@@ -0,0 +1,5 @@
+/Users/evan/Software/rust-workbook/Chapter01/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.rmeta: src/main.rs
+
+/Users/evan/Software/rust-workbook/Chapter01/hello_cargo/target/debug/deps/hello_cargo-6816878067fadf72.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter01/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/evan/Software/rust-workbook/Chapter01/hello_cargo/target/debug/hello_cargo.dSYM: /Users/evan/Software/rust-workbook/Chapter01/hello_cargo/src/main.rs
